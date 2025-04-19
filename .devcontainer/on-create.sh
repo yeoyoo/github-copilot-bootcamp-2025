916234ea025990bd14db42ca94a84bdd73fe4197
Justin Yoo <justin.yoojh@gmail.com>
2025-04-19 05:18:23 +0000

    Update devcontainer settings

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -8,25 +8,7 @@ echo Configure git
 git config --global pull.rebase false
 git config --global core.autocrlf input
 
-echo Update .NET workloads
-dotnet workload update --from-previous-sdk
-
 echo Install .NET dev certs
 dotnet dev-certs https --trust
-
-echo Install Aspire 9 templates
-dotnet new install Aspire.ProjectTemplates
-
-echo Install Azure Bicep CLI
-az bicep install
-
-echo Install Spring Boot CLI
-wget -P ~/ https://repo.maven.apache.org/maven2/org/springframework/boot/spring-boot-cli/3.4.4/spring-boot-cli-3.4.4-bin.zip
-unzip -o ~/spring-boot-cli-3.4.4-bin.zip -d ~/.spring
-mkdir -p ~/.spring/current
-cp -r ~/.spring/spring-3.4.4/* ~/.spring/current/
-
-echo 'export PATH="$PATH:$HOME/.spring/current/bin"' >> ~/.bashrc
-# source ~/.bashrc
 
 echo Done!
